0e602d193c21cdd2c458bff393aa49a696f608e4
cuxmol <cxumol@gmail.com>
2026-04-10 03:56:57 +0800

    init: config, etc.

---
+++ src/pennyparse/cli.py
@@ -0,0 +1,20 @@
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer(name="pennyparse", help="PennyParse CLI - Agentic Document Parser")
+
+
+@app.command()
+def init(name: Annotated[str, typer.Argument()] = "World"):
+    """Initialize prompts with current config files."""
+    pass
+
+
+@app.command()
+def serve(port: Annotated[int, typer.Option()] = 8000):
+    """Start the web server."""
+    pass
+
+
+if __name__ == "__main__":
+    app()
